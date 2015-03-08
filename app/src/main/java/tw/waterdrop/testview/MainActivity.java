d9079bb934961bb9e08a2a0965f5c22064cd9670
colon <colon.song@gmail.com>
2015-03-08 18:07:58 +0800

    基本架構完成

--- app/src/main/java/tw/waterdrop/testview/MainActivity.java
+++ app/src/main/java/tw/waterdrop/testview/MainActivity.java
@@ -1,39 +1,57 @@
 package tw.waterdrop.testview;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
+
+    private static final SampleTest[] samplesArray = new SampleTest[]{
+            new SampleTest(R.string.test1,Test1.class),
+            new SampleTest(R.string.test2,Test2.class)
+    };
+
+    private static class SampleTest{
+
+        final int title;
+        final Class targetClass;
+
+        SampleTest(int title,Class targetClass)
+        {
+            this.title = title;
+            this.targetClass = targetClass;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getTitlesList()));
+
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onListItemClick(ListView l,View v, int position, long id)
+    {
+        if (position >= 0 && position < samplesArray.length)
+        {
+            startActivity(new Intent(this, samplesArray[position].targetClass));
         }
-
-        return super.onOptionsItemSelected(item);
     }
+    private List<String> getTitlesList(){
+        List<String> titles = new ArrayList<String>();
+        for(SampleTest config: samplesArray)
+        {
+            titles.add(getString(config.title));
+        }
+        return titles;
+    }
+
+
 }
---
+++ app/src/main/java/tw/waterdrop/testview/Test2.java
@@ -0,0 +1,7 @@
+package tw.waterdrop.testview;
+
+/**
+ * Created by colon on 2015/3/8.
+ */
+public class Test2 {
+}
